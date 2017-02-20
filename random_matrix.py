d695cc17634329e418f357dd8be43c13d85585b9
invader88 <andrey.y21111@gmail.com>
2017-02-21 01:16:40 +0200

    Update random_matrix.py

--- random_matrix.py
+++ random_matrix.py
@@ -3,7 +3,7 @@ n = int(input("Enter dimension of matrix: "))
 def random_m(a):
     b = [[0 for _ in range(a)] for _ in range(a)]
     for i in range(a):
-        for j in range(n):
+        for j in range(a):
             b[i][j] = random.randint(100,999)
         print('{}'.format(b[i],[j]))
 random_m(n)
