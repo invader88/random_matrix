0868a7887443703aa095863a19dd7aaa52c724ba
invader88 <andrey.y21111@gmail.com>
2017-02-22 23:48:32 +0200

    Update random_matrix.py

--- random_matrix.py
+++ random_matrix.py
@@ -1,9 +1,14 @@
 import random
-n = int(input("Enter dimension of matrix: "))
+
 def random_m(a):
+    
     b = [[0 for _ in range(a)] for _ in range(a)]
     for i in range(a):
         for j in range(a):
             b[i][j] = random.randint(100,999)
         print('{}'.format(b[i],[j]))
-random_m(n)
+    return b
+        
+if __name__ == '__main__':       
+    n = int(input("Enter dimension of matrix: "))
+    random_m(n)
